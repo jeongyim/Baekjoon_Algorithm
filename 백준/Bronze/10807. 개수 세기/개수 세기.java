03d69c9080745d3a26cf5cd1ce15b3d13a8b82f0
jeongyim <80804295+jeongyim@users.noreply.github.com>
2024-05-20 16:27:50 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -29,7 +29,7 @@ public class Main {
                 count++;
             }
         }
-        System.out.println(count);
+        bw.write(Integer.toString(count));
 
         br.close();
         bw.flush();
