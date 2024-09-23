5720d83cfd7c9d40828fc74b4c5ee027f69b0e49
jeongyim <80804295+jeongyim@users.noreply.github.com>
2024-09-23 11:45:30 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1268 ms, Memory: 129128 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,22 +1,29 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		//Collections.sort()사용할 경우-> 배열X, list계열 사용
 		
 		for(int k = 0; k < N; k++) {
-			arr[k] = Integer.parseInt(br.readLine());
+			list.add(Integer.parseInt(br.readLine()));
 		}
-		Arrays.sort(arr);
+		//Arrays.sort(arr); _최악의 경우 n^2 걸림
+		Collections.sort(list); 
+		//timsort 합병정령,삽입정렬인 hybrid sorting 알고리즘 사용
 		
-		for(int i = 0; i < N; i++) {
-			System.out.println(arr[i]);
+		for(int result : list) {
+			sb.append(result).append('\n');
 		}
+		System.out.println(sb);
 	}
 }
