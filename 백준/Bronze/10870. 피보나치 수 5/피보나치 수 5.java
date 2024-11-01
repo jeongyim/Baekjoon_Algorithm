ea40cba38cc72e10e311de1da8078f2a36f141c4
jeongyim <80804295+jeongyim@users.noreply.github.com>
2024-11-01 16:26:03 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 180 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+
+		System.out.println(fivo(N));
+	}
+	
+	static int fivo(int n) {
+		
+		if(n == 0) {
+			return n;
+		}
+		else if(n ==1) {
+			return 1;
+		}
+		
+		return fivo(n - 1) + fivo(n - 2);
+	}
+
+}
