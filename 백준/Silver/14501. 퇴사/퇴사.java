913f043668ad38d2481374a5d1c9475fe6a69b98
jeongyim <80804295+jeongyim@users.noreply.github.com>
2024-11-11 17:38:18 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] t = new int[n];
+		int[] p = new int[n];
+		
+		StringTokenizer st;
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			t[i] = Integer.parseInt(st.nextToken());
+			p[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		//dp:N이에 얻을 수 있는 최대 수익
+		int[] dp = new int[n+1];
+		
+		for(int i = 0; i < n; i++) {
+			if(i + t[i] <= n) {
+				dp[i+t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
+			}
+			dp[i+1] = Math.max(dp[i+1], dp[i]);
+		}
+	System.out.println(dp[n]);
+		
+	}
+
+}
