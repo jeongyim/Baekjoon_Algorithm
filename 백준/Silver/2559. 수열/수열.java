575bf860fd674b725332a12a62baedd0ed5e30b0
jeongyim <80804295+jeongyim@users.noreply.github.com>
2025-05-24 12:15:31 +0900

    [Silver III] Title: 수열, Time: 284 ms, Memory: 23836 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] A = new int[N+1];
+		int[] S = new int[N+1];
+		int max = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+			S[i] = S[i-1] + A[i];
+			max = S[K];
+		}
+
+		int end = K;
+		while(end <= N) {
+			int sum = S[end] - S[end-K];
+			if(sum > max) {
+				max = sum;
+			}
+			end++;
+		}
+		System.out.println(max);
+	
+	}
+
+}
